43f965917da0d01cd7d4048daaef432a2e35b912
Kevin Ji <kevin.ji@outlook.com>
2016-01-10 01:10:05 -0800

    angle-printer: Simplify and clean up code
    Removed the unneeded class, added whitespace, and simplified logic.

--- angle-printer.js
+++ angle-printer.js
@@ -1,20 +1,18 @@
-function AnglePrinter(){
-	//http://stackoverflow.com/a/17445304/896112 - Recursive Euclid's Algorithm
-	this.gcd = function gcd(a, b) {
-		return (!b)?a:gcd(b, a%b);
-	}
+// http://stackoverflow.com/a/17445304/896112 - Recursive Euclid's Algorithm
+function gcd(a, b) {
+	return (!b) ? a : gcd(b, a % b);
 }
 
-AnglePrinter.prototype.toRadians = function(degrees){
+var anglePrinter = {};
 
+anglePrinter.toRadians = function(degrees) {
 	// Account for integer coefficients of degrees / 180
-	if(degrees % 180 == 0) {
-		if(degrees == 180) {
+	if (degrees % 180 === 0) {
+		if (degrees === 180) {
 			return "π";
-		}
-		else if(degrees == -180){
+		} else if (degrees === -180) {
 			return "-π";
-		}else{
+		} else {
 			return degrees / 180 + "π";
 		}
 	}
@@ -23,47 +21,46 @@ AnglePrinter.prototype.toRadians = function(degrees){
 	var frac = degrees / 180;
 
 	// 10 ** number of decimal places
-	var multiplier = Math.pow(10, frac.toString().split(".")[1].length);
+	var denominator = Math.pow(10, frac.toString().split(".")[1].length);
 	
-	// Multiply top and bottom by 10^n to get integer numerator and denominator
-	var numerator = frac * multiplier;
-	var denominator = multiplier;
+	// Multiply top by 10^n to get integer numerator
+	var numerator = frac * denominator;
 
 	// Find GCD (or GCF) of numerator and denominator to reduce fraction
-	var g = this.gcd(numerator, denominator);	
+	var g = gcd(numerator, denominator);	
 
 	// Reduce fraction
-	numerator = numerator/g;
-	denominator = denominator/g;
+	numerator /= g;
+	denominator /= g;
+	
+	// If the denominator is a negative number, move negative sign to numerator
+	if (denominator < 0) {
+		denominator = -denominator;
+		numerator = -numerator;
+	}
 
 	// This is a pretty printer, replaces 1 or -1 with π or -π
-	if(numerator == 1){
-		numerator = "π";
-	}else if(numerator == -1){
-		numerator = "-π";
-	}else{
-		numerator += "π"
+	if (numerator === 1) {
+		numerator = "";
+	} else if (numerator === -1) {
+		numerator = "-";
 	}
+	
+	numerator += "π";
 
-	// If the denominator is a negative number, move negative sign to numerator
-	if(denominator < 0){
-		denominator = Math.abs(denominator);
-		numerator = "-"+numerator;
-	}
+	return numerator + "/" + denominator; 
+};
 
-	return numerator +"/"+ denominator; 
-}
+anglePrinter.toDegrees = function(radians) {
+	return radians * (180 / Math.PI) + "°";
+};
 
-AnglePrinter.prototype.toDegrees = function(radian){
-	return radian * (180/Math.PI) + "°";
-}
+anglePrinter.printRadians = function(radians) {
+	return this.toRadians(radians * (180 / Math.PI));
+};
 
-AnglePrinter.prototype.printRadians = function(radian){
-	return this.toRadians(radian * (180/Math.PI));
-}
+anglePrinter.printDegrees = function(degrees) {
+	return degrees + "°";
+};
 
-AnglePrinter.prototype.printDegrees = function(degrees){
-	return degrees +"°";
-}
-
-module.exports = new AnglePrinter();
+module.exports = anglePrinter;
